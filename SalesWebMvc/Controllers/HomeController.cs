83b2644a8adf2b213e7cc657c263857563f9501f
Gustavo Sena <123789443+gustavosenamp@users.noreply.github.com>
2024-07-07 13:46:24 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course.";
+            ViewData["email"] = "gustavosena07052005@gmail.com";
+            ViewData["Nome"] = "Gustavo Sena";
 
             return View();
         }
